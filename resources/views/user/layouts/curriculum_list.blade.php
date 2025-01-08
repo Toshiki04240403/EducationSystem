015dc8292b522c510d668e1d1fc31e7c8473fd70
Toshiki04240403 <toshiki04240403+1@gmail.com>
2025-01-08 14:36:20 +0900

    1/8curriclum

--- resources/views/user/layouts/curriculum_list.blade.php
+++ resources/views/user/layouts/curriculum_list.blade.php
@@ -1,37 +1,66 @@
-<!-- resources/views/user/layouts/curriculum_list.blade.php -->
 <!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>カリキュラムリスト</title>
-    @vite(['resources/css/app.css']) <!-- CSSファイルへのリンク -->
+    @vite(['resources/css/app.css'])
 </head>
 <body>
     <header>
         <nav>
             <ul>
-                <li><a href="{{ route('curriculums.index') }}">時間割</a>
-                <a href="/progress">授業進捗</a>
-                <a href="/profile">プロフィール設定</a>
-                <a href="/logout">ログアウト</a>
-                <li><a href="/back">戻る</a></li>
+                <li><a href="{{ route('curriculums.index') }}">時間割</a></li>
+                <li><a href="/progress">授業進捗</a></li>
+                <li><a href="/profile">プロフィール設定</a></li>
+                <li><a href="/logout">ログアウト</a></li>
             </ul>
         </nav>
     </header>
-
-       <div class="container">
+    <a href="/back">←戻る</a>
+    <div class="derivery-time">
+                <button>◀</button>
+                <p>〇〇年〇〇月スケジュール</p>
+                <button>▶</button>
+    </div>
+    
+    <div class="container">
         <div class="flex-container">
-        <div class="button-group">
-            @foreach($grades as $grade)
-                <button class="grade-button">{{ $grade->name }}</button>
-            @endforeach
-        </div>
-       </div>
-                   
+            <div class="button-group">
+                @foreach($grades as $grade)
+                    @php
+                        $buttonClass = '';
+                        switch ($grade->name) {
+                        case '小学1年生':
+                        case '小学2年生':
+                        case '小学3年生':
+                        case '小学4年生':
+                        case '小学5年生':
+                        case '小学6年生':
+                            $buttonClass = 'grade-button grade-1';
+                            break;
+                        case '中学1年生':
+                        case '中学2年生':
+                        case '中学3年生':
+                            $buttonClass = 'grade-button grade-2';
+                            break;
+                        case '高校1年生':
+                        case '高校2年生':
+                        case '高校3年生':
+                            $buttonClass = 'grade-button grade-3';
+                            break;
+                        default:
+                            $buttonClass = 'grade-button';
+                            break;
+                        }
+                    @endphp
+                    <button class="{{ $buttonClass }}">{{ $grade->name }}</button>
+                @endforeach
+            </div>
+            
+                
 
-               
-            <div class="curriculum-list" >
+            <div class="curriculum-list">
                 @foreach ($curriculums as $curriculum)
                     <div class="curriculum-item">
                         <img src="{{ $curriculum->thumbnail }}" alt="{{ $curriculum->title }}">
@@ -41,5 +70,6 @@
                 @endforeach
             </div>
         </div>
+    </div>
 </body>
 </html>
